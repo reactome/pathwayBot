38fd50e2c9ce6afe70b325de838fcb80d79a9e62
Fatemeh Almodaresi <sfmodarres@gmail.com>
2023-09-19 16:18:12 -0400

    Changing the sorting criteria for Entrez to most-relevant

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@ python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
 
-python main.py
-
 #### sample commands:
 ### download 8 relevant documents in PMC (--oa) to each query in file "query_test12" and save metadata and the document text in dir pmid_out12
-/usr/bin/time python ../src/main.py find_add_pmids --file query_test12.lst --outdir pmid_out12 --oa --num_pmids 8
+mkdir -p tests/pmid_out12 && /usr/bin/time python src/main.py find_add_pmids --file tests/query_test12.lst --outdir tests/pmid_out12 --oa --num_pmids 8
+
 ### if remove --oa, it will not download the text and only store the abstract, will also search in pubmed instead of PMC
-/usr/bin/time python ../src/main.py find_add_pmids --file query_test12.lst --outdir pmid_out12 --num_pmids 50
+mkdir -p tests/pmid_out12 && /usr/bin/time python src/main.py find_add_pmids --file tests/query_test12.lst --outdir tests/pmid_out12 --num_pmids 50
+
 ### sample to generate summaries for a list of queries (pathways) stored in a file, separated by newline
-python ../src/main.py summarize_pathway_batch --file query_test1.lst --outdir pmid_out1 --kg_file ../data/kg/reactome2langChainGraph2023.gml 
+mkdir -p tests/pmid_out12 && /usr/bin/time python src/main.py summarize_pathway_batch --file tests/query_test12.lst --outdir tests/pmid_out12 --kg_file data/kg/reactome2langChainGraph2023.gml 
--- src/paper_manager.py
+++ src/paper_manager.py
@@ -5,6 +5,9 @@ import time
 from metapub.cite import *
 import pandas as pd
 
+from Bio import Entrez
+
+
 ## todo: what ever format we support for the knowledgebase of Reactome goes in here
 class PaperManager:
     def __init__(self, pdfRoot="papers"):
@@ -17,12 +20,24 @@ class PaperManager:
         # Initialize the list of papers
         #self.papers = self.paperScraper.metadf
 
+    def search_by_query(self, query, retmax=5, pmc_only=False, sort="relevance"):
+
+        Entrez.email = "sfmodarres@gmail.com"
+        if pmc_only:
+            handle = Entrez.esearch(db="pubmed", term=query + " AND pubmed pmc open access[filter]", retmax=retmax, sort=sort)
+        else:
+            handle = Entrez.esearch(db="pubmed", term=query, retmax=retmax, sort=sort)
+        record = Entrez.read(handle)
+        handle.close()
+        return record["IdList"]
+
     def get_papers_for_query(self, pmidFile, num_pmids=3, pmc_only=True):
         with open(pmidFile) as f:
             for query in f:
                 query = query.strip()
                 print(query)
-                pmidList = PubMedFetcher().pmids_for_medical_genetics_query(query=query, retmax=num_pmids, pmc_only=pmc_only)
+                #pmidList = PubMedFetcher().pmids_for_medical_genetics_query(query=query, retmax=num_pmids, pmc_only=pmc_only)
+                pmidList = self.search_by_query(query, retmax=num_pmids, pmc_only=pmc_only)
                 if not pmc_only:
                     time.sleep(10)
                 print("pmids:", pmidList)
